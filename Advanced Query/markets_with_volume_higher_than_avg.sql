12e586c218f755f540ac9a301a436ae9191a4b43
minhchantran02 <minhchan.tran@sjsu.edu>
2026-03-19 22:13:49 -0700

    Delete Advanced Query directory

--- Advanced Query/markets_with_volume_higher_than_avg.sql
+++
@@ -1,5 +0,0 @@
-#which markets have volume higher than the average volume?
-select question, round(volume, 2) as volume
-from markets
-where volume > (select avg(volume) from markets)
-order by volume desc;
