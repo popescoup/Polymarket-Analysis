6a5cdd614dd7b1684288654cb5e267336fe23fcc
minhchantran02 <minhchan.tran@sjsu.edu>
2026-03-19 22:14:08 -0700

    Delete Simple Query directory

--- Simple Query/most_active_maker_by_trades.sql
+++
@@ -1,5 +0,0 @@
-#top 5 most active makers by number of trades
-select maker, total_trades, round(total_volume, 2) as total_volume
-from maker
-order by total_trades desc
-limit 5;
--- Simple Query/top_5_highest_volume_market.sql
+++
@@ -1,4 +0,0 @@
-#Top 5 highest volume markets 
-select question, volume
-from markets
-order by volume desc limit 5;
